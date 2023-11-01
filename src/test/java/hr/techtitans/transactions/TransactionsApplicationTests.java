eb4c6beaa05af4bbd468d6f2b9914237db12d304
tsopic3008 <tin.sopic@gmail.com>
2023-11-01 19:41:58 +0100

    first commit

---
+++ src/test/java/hr/techtitans/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.techtitans.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
